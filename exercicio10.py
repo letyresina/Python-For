9eff54a78f42fd6f8377b246d2a457108dd906a4
letyresina <leticiaresina@gmail.com>
2023-04-25 09:02:04 -0300

    Subindo exercício 10

--- exercicio10.py
+++ exercicio10.py
@@ -1,4 +1,16 @@
 '''
     Exercício 10:
-    
+    Escreva um algoritmo que determine se um número N (digitado pelo usuário) é primo ou não.
 '''
+
+N = int(input("Informe um número inteiro qualquer: "))
+multiplo = 0
+
+for i in (2, N + 1):
+    if (N % i == 0):
+        multiplo += 1
+
+if (multiplo == 0):
+    print(f"O número {N} é primo")
+else:
+    print(f"O número {N} não é primo")
--- exercicio4.py
+++ exercicio4.py
@@ -1,3 +1,8 @@
+'''
+    Exercício 4:
+    Escreva um algoritmo que leia quinze números informados pelo usuário e exiba a raiz quadrada 
+    de cada número (Dica: utilize a biblioteca math)
+'''
 # Importando a biblioteca math
 import math
 
