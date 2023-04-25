36264e4ec7d06f3caf5483a9ce50e58fdbc5d01e
letyresina <leticiaresina@gmail.com>
2023-04-25 09:04:33 -0300

    Colocado comentário no exercício 5 e pequeno ajuste

--- exercicio5.py
+++ exercicio5.py
@@ -1,3 +1,9 @@
+'''
+    Exercício 5:
+    Solicite a quantidade de alunos de uma turma e a quantidade de notas. Para cada aluno, 
+    solicite as suas notas e exiba a sua respectiva média (a média deve ser arredondada para duas 
+    casas decimais).
+'''
 quantidadeAlunos = int(input("Informe a quantidade de alunos: "))
 quantidadeNotas = int(input("Informe a quantidade de nota de cada aluno "))
 
@@ -7,4 +13,4 @@ for i in range(quantidadeAlunos):
         nota = int(input("Informe a nota do aluno: "))
         somaNota += nota
     media = somaNota / quantidadeNotas
-    print(f"A média desse aluno é de {media}")
+    print(f"A média desse aluno é de {media:.2f}")
